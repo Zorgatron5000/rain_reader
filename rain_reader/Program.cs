d711d178daaa984ba24a4fa9a3265c28321836c8
Zorgatron5000 <mrzorg@gmail.com>
2022-05-20 12:17:08 +0800

    Added original data files for ease of testing

--- rain_reader/Program.cs
+++ rain_reader/Program.cs
@@ -2,22 +2,24 @@
 using System.Reflection;
 using System.IO;
 using System.Data;
-
+using CsvHelper;
+using System.Globalization;
 
 // grab the folder location
 
 string FolderLocation;
-string CurrentFile;
+
+FolderLocation = ""; //because compilers are silly now
 
 //grab the location of the datafiles from the first args value, check its valid or exit and say why.
-if (args.Length = 0)
+if (args.Length == 0)
 {
     Console.WriteLine("Please specifiy the path to the datafiles in the first argument");
     Environment.Exit(1);
 }
 else
 {
-    FolderLocation = args[0];
+    FolderLocation = args[1];
     if (!Directory.Exists(FolderLocation))
     {
         Console.WriteLine("Specified folder does not exist: {0}", FolderLocation);
@@ -39,21 +41,35 @@ GaugeReadings.Columns.Add("Rainfall", typeof(int));
 
 //load the gauge info
 using (var reader = new StreamReader(FolderLocation + "Devices.csv"))
+
+using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
 {
+    // load the Guage info into the table
+    using (var dr = new CsvDataReader(csv))
+    {
+        Gauges.Load(dr);
+        //this is such a cheat, should add some kind of error checking
+
+    }
+}
+
+//load all the gauge readings
+string[] files = Directory.GetFiles(FolderLocation, "Data*.txt");
+
+foreach (var myFile in files)
+{
+    using (var reader = new StreamReader(FolderLocation + "Devices.csv"))
     using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
     {
-        // load the Guage info into the table
+        // load the Guage Readings info into the table
         using (var dr = new CsvDataReader(csv))
         {
-            Gauges.Load(dr);
+            GaugeReadings.Load(dr);
             //this is such a cheat, should add some kind of error checking
 
         }
     }
 }
 
-//load all the gauge readings
 
-//enumerate the files that might contain info
-//slurp them up into the table
 
