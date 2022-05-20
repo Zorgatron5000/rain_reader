697a7b2999a51f85ba9d3254f4542d6dad4e1fdc
Zorgatron5000 <mrzorg@gmail.com>
2022-05-20 10:48:43 +0800

    Basic outline of intended processing and some preliminary dependencies

--- rain_reader/Program.cs
+++ rain_reader/Program.cs
@@ -1,1 +1,11 @@
 ﻿
+// grab the folder location
+// load gauges file
+// run through the list of gauges, check basic errors
+// 
+// check for data files
+// process data files
+// assume reading all info into data structure to be filtered afterwards
+// once all datafiles scraped run the interface
+//      for each gauge, display AVG and add trend indicator
+// exit
---
+++ rain_readerTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace rain_reader
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
