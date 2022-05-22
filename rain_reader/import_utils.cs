0529808dce8b499548a339878d98eb8dcd76eeb9
Zorgatron5000 <mrzorg@gmail.com>
2022-05-22 16:10:46 +0800

    abstracted the import process and a debug check so we could do some unit testing examples

---
+++ rain_reader/import_utils.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+using System.Globalization;
+using CsvHelper;
+
+namespace utils;
+
+public static partial class Utils
+{
+    public static bool LoadFileIntoTable(string filename, DataTable table)
+    {
+        using (var reader = new StreamReader(filename))
+        {
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                // load the Guage info into the table
+                using (var dr = new CsvDataReader(csv))
+                {
+                    try
+                    {
+                        table.Load(dr);
+                        //this is such a cheat, should add some kind of error checking
+                        //need to filter out error values like blank lines. although current docs indicate that this is supposed to be the case but quick testing of loading the files indicates otherwise.
+                        //will need to check CsvHelper version specs against documentation. in the meantime I have removed the blank line in the Devices.txt file to skip the error condition.
+                        return true;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void OutputToScreen(string title, DataRowCollection show_results)
+    {
+        Console.WriteLine(title);
+        Console.WriteLine();
+
+        foreach (DataRow dataRow in show_results)
+        {
+            foreach (var item in dataRow.ItemArray)
+            {
+                Console.Write("{0}, ", item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ rain_readerTests/UtilsTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace utils.Tests
+{
+    [TestClass()]
+    public class UtilsTests
+    {
+        [TestMethod()]
+        public void OutputToScreenTest()
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            String[] arr = new String[] { "1", "2", "3" };
+            
+            var dt = new DataTable();
+            dt.Columns.Add("First");
+            dt.Columns.Add("Second");
+            dt.Columns.Add("Third");
+
+            dt.Rows.Add(arr);
+            dt.Rows.Add(arr);
+
+
+            Utils.OutputToScreen("Banana", dt.Rows);
+
+            Assert.AreEqual("Banana\r\n\r\n1, 2, 3, \r\n1, 2, 3, \r\n", stringWriter.ToString());
+        }
+        [TestMethod()]
+        public void OutputToScreenTestBlankValue()
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            String[] arr = new String[] { "1", "2" };
+
+            var dt = new DataTable();
+            dt.Columns.Add("First");
+            dt.Columns.Add("Second");
+            dt.Columns.Add("Third");
+
+            dt.Rows.Add(arr);
+
+
+            Utils.OutputToScreen("Banana", dt.Rows);
+
+            Assert.AreEqual("Banana\r\n\r\n1, 2, , \r\n", stringWriter.ToString());
+        }
+        [TestMethod()]
+        public void OutputToScreenTestNoValues()
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            String[] arr = new String[] { "1", "2" };
+
+            var dt = new DataTable();
+            dt.Columns.Add("First");
+            dt.Columns.Add("Second");
+            dt.Columns.Add("Third");
+
+            Utils.OutputToScreen("Banana", dt.Rows);
+
+            Assert.AreEqual("Banana\r\n\r\n", stringWriter.ToString());
+        }
+    }
+}
